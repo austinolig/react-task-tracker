2ae3cefdec58d3a809ede7c2bf71ecc6f4f7f6d7
Austin Oligario <austinoligario@gmail.com>
2021-06-24 19:05:47 -0400

    Changed localhost REST API to 'My JSON Server' API

--- src/App.js
+++ src/App.js
@@ -22,14 +22,18 @@ const App = () => {
 
   // fetch tasks
   const fetchTasks = async () => {
-    const res = await fetch("http://localhost:5000/tasks");
+    const res = await fetch(
+      "http://my-json-server.typicode.com/AustinOlig/react-task-tracker/tasks"
+    );
     const data = await res.json();
 
     return data;
   };
 
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const res = await fetch(
+      `http://my-json-server.typicode.com/AustinOlig/react-task-tracker/tasks/${id}`
+    );
     const data = await res.json();
 
     return data;
@@ -38,7 +42,10 @@ const App = () => {
   // Delete Task
   const deleteTask = async (id) => {
     console.log("Delete!", id);
-    await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+    await fetch(
+      `http://my-json-server.typicode.com/AustinOlig/react-task-tracker/tasks/${id}`,
+      { method: "DELETE" }
+    );
 
     setTasks(tasks.filter((task) => task.id !== id));
   };
@@ -50,13 +57,16 @@ const App = () => {
     const taskToToggle = await fetchTask(id);
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(updTask),
-    });
+    const res = await fetch(
+      `http://my-json-server.typicode.com/AustinOlig/react-task-tracker/tasks/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      }
+    );
 
     const data = await res.json();
 
@@ -73,13 +83,16 @@ const App = () => {
     // const newTask = { id, ...task }
     // console.log('Add!', newTask)
     // setTasks([...tasks, newTask])
-    const res = await fetch("http://localhost:5000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(task),
-    });
+    const res = await fetch(
+      "http://my-json-server.typicode.com/AustinOlig/react-task-tracker/tasks",
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      }
+    );
 
     const data = await res.json();
 
